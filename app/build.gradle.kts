a260acb55a5c3e6e47ad72bc8c962009cd7706ac
Tarbolin69 <nicolas@solcaronte.xyz>
2026-05-25 16:25:40 -0300

    SplashScreen: SplashScreen inicial con logo temporario nativo de Android (no se como sacar el SplashScreen nativo de Android Studio)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
