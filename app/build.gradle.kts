451ccf0064200c79a26c8fe7f2472758378d983e
Tarbolin69 <nicolas@solcaronte.xyz>
2026-05-25 15:50:17 -0300

    SplashScreen: inicializando archivos entre otras cosas primero.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/libreria/alexandria/components/splash/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.libreria.alexandria.components.splash
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+fun SplashScreen(
+    navController: NavHostController,
+    modifider: Modifier = Modifier
+){
+
+}
