b718f325f6f19683468f12313a17172783a478b3
Tarbolin69 <tarbolin69@gmail.com>
2026-05-26 13:39:54 -0300

    CAMBIOS UI: Fuentes tipograficas añadidas al theme del app.

--- app/src/main/java/com/libreria/alexandria/ui/theme/Type.kt
+++ app/src/main/java/com/libreria/alexandria/ui/theme/Type.kt
@@ -2,16 +2,41 @@ package com.libreria.alexandria.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.libreria.alexandria.R
 
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+object AppFont {
+    val Aporetic = FontFamily(
+        Font(R.font.aporetic_sans_regular, FontWeight.Normal),
+        Font(R.font.aporetic_sans_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.aporetic_sans_bold, FontWeight.Bold),
+        Font(R.font.aporetic_sans_bold_italic, FontWeight.Bold, FontStyle.Italic)
     )
+}
+
+private val defaultTypography = Typography()
+val Typography = Typography(
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = AppFont.Aporetic),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = AppFont.Aporetic),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = AppFont.Aporetic),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = AppFont.Aporetic),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = AppFont.Aporetic),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = AppFont.Aporetic),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = AppFont.Aporetic),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = AppFont.Aporetic),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = AppFont.Aporetic),
+
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = AppFont.Aporetic),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = AppFont.Aporetic),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = AppFont.Aporetic),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = AppFont.Aporetic),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = AppFont.Aporetic),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = AppFont.Aporetic)
 )
